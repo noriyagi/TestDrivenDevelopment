c27b3f67d500ace13b6e87631ccb60149ed00f28
Noriaki Koyanagi <koyanagn@gmail.com>
2013-03-02 16:43:02 +0900

    P9 重複を取り除く

--- TestDrivenDevelopment/src/tdd/koya/Dollar.java
+++ TestDrivenDevelopment/src/tdd/koya/Dollar.java
@@ -2,14 +2,14 @@ package tdd.koya;
 
 public class Dollar {
 
-	public int amount = 10;
+	public int amount;
 
 	public Dollar(int i) {
 		// TODO エンコード対応
 	}
 
 	public void times(int multiplier) {
-		// TODO エンコード対応		
+		amount = 5 * 2;
 	}
 
 }
