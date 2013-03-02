3d9389ec25c61e8f3de81afd3678f61cad01bea6
Noriaki Koyanagi <koyanagn@gmail.com>
2013-03-02 15:48:25 +0900

    P6 最初のテスト成功

---
+++ TestDrivenDevelopment/src/tdd/koya/Dollar.java
@@ -0,0 +1,16 @@
+package tdd.koya;
+
+public class Dollar {
+
+	public int amount = 10;
+
+	public Dollar(int i) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public void times(int multiplier) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
