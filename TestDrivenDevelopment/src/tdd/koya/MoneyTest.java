bb745cca990a12a616722a3ec72b6b8f50130923
Noriaki Koyanagi <koyanagn@gmail.com>
2013-03-10 20:53:30 +0900

    P40 通貨に関するテスト作成

--- TestDrivenDevelopment/src/tdd/koya/MoneyTest.java
+++ TestDrivenDevelopment/src/tdd/koya/MoneyTest.java
@@ -28,5 +28,11 @@ public class MoneyTest {
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 	}
+	// 通貨についてのテスト
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency);
+		assertEquals("CHF", Money.franc(1).currency);
+	}
 
 }
