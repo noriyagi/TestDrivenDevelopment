6c686b56ae46ba78582469cdd94b7e8df8d00f5d
Noriaki Koyanagi <koyanagn@gmail.com>
2013-03-16 16:23:00 +0900

    P53 通貨に基づくロジックが存在しないのでtestFrancMultiplication()を削除する

--- TestDrivenDevelopment/src/tdd/koya/MoneyTest.java
+++ TestDrivenDevelopment/src/tdd/koya/MoneyTest.java
@@ -19,13 +19,7 @@ public class MoneyTest {
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertFalse(Money.dollar(5).equals(Money.franc(5)));
 	}
-	
-	@Test
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
+
 	// 通貨についてのテスト
 	@Test
 	public void testCurrency() {
