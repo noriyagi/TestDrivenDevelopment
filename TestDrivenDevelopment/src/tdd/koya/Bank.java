f8ed2e9fdc9e01d60f6caf5f825b35fef7bea5a3
Noriaki Koyanagi <koyanagn@gmail.com>
2013-03-24 15:59:30 +0900

    P65 余分なキャスト、クラスチェック(instanceof)を削除

--- TestDrivenDevelopment/src/tdd/koya/Bank.java
+++ TestDrivenDevelopment/src/tdd/koya/Bank.java
@@ -3,8 +3,6 @@ package tdd.koya;
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
-		if (source instanceof Money) return (Money)source.reduce(to);
-		Sum sum = (Sum)source;
-		return sum.reduce(to);
+		return source.reduce(to);
 	}
 }
